ef5b64327d8e50a2142a7b17d8cb006ff06b06ca
azizahtrikd <19220986@bsi.ac.id>
2023-10-09 15:05:52 +0700

    mengedit config, menambahkah file (web, header, about, index, footer, css)

--- application/config/config.php
+++ application/config/config.php
@@ -5,7 +5,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |--------------------------------------------------------------------------
 | Base Site URL
 |--------------------------------------------------------------------------
-|
+|]'
 | URL to your CodeIgniter root. Typically this will be your base URL,
 | WITH a trailing slash:
 |
